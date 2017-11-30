8a04c85cdb5bc4f8a645fa4a4a80335bef13e77f
b6021643 <b6021643@9309-26J522044>
2017-11-30 16:45:01 +0000

    Add project files.

---
+++ CarParkSystem/Form1.Designer.cs
@@ -0,0 +1,295 @@
+﻿namespace CarParkSystem
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.btnEntry = new System.Windows.Forms.Button();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.groupBox5 = new System.Windows.Forms.GroupBox();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.tbxLicense = new System.Windows.Forms.TextBox();
+            this.tbxPassCode = new System.Windows.Forms.TextBox();
+            this.btnNewCar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox5.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.listBox1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 501);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Floor space";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(6, 45);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(188, 446);
+            this.listBox1.TabIndex = 0;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.BackColor = System.Drawing.SystemColors.Control;
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.btnNewCar);
+            this.groupBox2.Controls.Add(this.tbxPassCode);
+            this.groupBox2.Controls.Add(this.tbxLicense);
+            this.groupBox2.Controls.Add(this.button5);
+            this.groupBox2.Controls.Add(this.button2);
+            this.groupBox2.Controls.Add(this.btnEntry);
+            this.groupBox2.Location = new System.Drawing.Point(218, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(331, 163);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Car entry";
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(250, 19);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "Go Park";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(169, 19);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Get Coin";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // btnEntry
+            // 
+            this.btnEntry.Location = new System.Drawing.Point(6, 100);
+            this.btnEntry.Name = "btnEntry";
+            this.btnEntry.Size = new System.Drawing.Size(75, 23);
+            this.btnEntry.TabIndex = 0;
+            this.btnEntry.Text = "Entry";
+            this.btnEntry.UseVisualStyleBackColor = true;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.button8);
+            this.groupBox3.Controls.Add(this.button7);
+            this.groupBox3.Controls.Add(this.button6);
+            this.groupBox3.Location = new System.Drawing.Point(218, 350);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(331, 163);
+            this.groupBox3.TabIndex = 2;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Car Exit";
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(168, 19);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(75, 23);
+            this.button8.TabIndex = 2;
+            this.button8.Text = "Exit";
+            this.button8.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(87, 19);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(75, 23);
+            this.button7.TabIndex = 1;
+            this.button7.Text = "Return Coin";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(6, 19);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 0;
+            this.button6.Text = "Unpark";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Location = new System.Drawing.Point(555, 12);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(359, 501);
+            this.groupBox4.TabIndex = 2;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Payment and other externals";
+            // 
+            // groupBox5
+            // 
+            this.groupBox5.BackColor = System.Drawing.SystemColors.Control;
+            this.groupBox5.Controls.Add(this.button3);
+            this.groupBox5.Controls.Add(this.button10);
+            this.groupBox5.Controls.Add(this.button11);
+            this.groupBox5.Location = new System.Drawing.Point(218, 181);
+            this.groupBox5.Name = "groupBox5";
+            this.groupBox5.Size = new System.Drawing.Size(331, 163);
+            this.groupBox5.TabIndex = 5;
+            this.groupBox5.TabStop = false;
+            this.groupBox5.Text = "In park";
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(6, 48);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(75, 23);
+            this.button10.TabIndex = 3;
+            this.button10.Text = "go down floor";
+            this.button10.UseVisualStyleBackColor = true;
+            // 
+            // button11
+            // 
+            this.button11.Location = new System.Drawing.Point(6, 19);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(75, 23);
+            this.button11.TabIndex = 2;
+            this.button11.Text = "go up floor";
+            this.button11.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(6, 134);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Park";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // tbxLicense
+            // 
+            this.tbxLicense.Location = new System.Drawing.Point(6, 48);
+            this.tbxLicense.Name = "tbxLicense";
+            this.tbxLicense.Size = new System.Drawing.Size(100, 20);
+            this.tbxLicense.TabIndex = 5;
+            // 
+            // tbxPassCode
+            // 
+            this.tbxPassCode.Location = new System.Drawing.Point(6, 74);
+            this.tbxPassCode.Name = "tbxPassCode";
+            this.tbxPassCode.Size = new System.Drawing.Size(100, 20);
+            this.tbxPassCode.TabIndex = 6;
+            // 
+            // btnNewCar
+            // 
+            this.btnNewCar.Location = new System.Drawing.Point(6, 19);
+            this.btnNewCar.Name = "btnNewCar";
+            this.btnNewCar.Size = new System.Drawing.Size(75, 23);
+            this.btnNewCar.TabIndex = 7;
+            this.btnNewCar.Text = "New Car";
+            this.btnNewCar.UseVisualStyleBackColor = true;
+            this.btnNewCar.Click += new System.EventHandler(this.btnNewCar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(112, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "License Plate";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(112, 77);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Passcode";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(926, 525);
+            this.Controls.Add(this.groupBox5);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Car Park System";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox5.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button btnEntry;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.GroupBox groupBox5;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnNewCar;
+        private System.Windows.Forms.TextBox tbxPassCode;
+        private System.Windows.Forms.TextBox tbxLicense;
+    }
+}
+
---
+++ CarParkSystem/Program.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CarParkSystem
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Carpark carpark;
+            Application.Run(new Form1());
+        }
+    }
+    public class Carpark
+    {
+        private const int maxSpaces = 600;//make this generatable and linked to the other maxSpaces
+        private const int numOfFloors = 12;
+
+        private Car[] listOFCars = new Car[maxSpaces];
+        private List<Car> carList = new List<Car>();
+        private Floor[] floor = new Floor[numOfFloors];
+
+        private Enterance enterance;
+        private Exit exit;
+
+        private ChipMachine chipMachine;
+        private PaymentMachine paymentMachine;
+        private DiscountMachine discountMachine;
+        private ChipReader chipReader;
+
+    }
+    public class Floor
+    {
+        private const int floorSize = 50;
+        private bool[] carSpace = new bool[floorSize];
+
+
+    }
+    public class Enterance
+    {
+        //give new car, linsence plat num,
+        public void addNewCar(List<Car> carList,string licensePlate, string passcode)
+        {
+            Car newCar = new Car(licensePlate, passcode);
+            carList.Add(newCar);
+        }
+    }
+    public class Exit
+    {
+        //remove car from system.
+        public void removeCar(List<Car> carList, Car carToRemove)
+        {
+            carList.Remove(carToRemove);            
+        }
+    }
+    public class Machine
+    {
+        static private int maxSpaces = 600;//make this generatable and linked to the other maxSpaces
+        static protected PaymentChip[] ticketChip = new PaymentChip[maxSpaces];
+
+    }
+    public class ChipMachine : Machine
+    {
+        //create chips
+    }
+    public class DiscountMachine : Machine
+    {
+        //adds discount to a chip
+
+    }
+    public class PaymentMachine : Machine
+    {
+        //commit payment and remove chips
+    }
+    public class ChipReader : Machine
+    {
+
+    }
+    public class PaymentChip
+    {
+        private int iD { get; }
+        private DateTime startTime { get; }
+        private int discount { get; set; }
+        public PaymentChip(int iD)
+        {
+            this.iD = iD;
+            startTime = DateTime.Now;
+        }
+        public void applyDiscount(int discount)
+        {
+            this.discount += discount;
+        }
+    }
+    public class Car
+    {
+        private string licensePlate { get;}
+        private int floor { get; set; }
+        private int place { get; set; }
+        private string passCode { get;}
+
+        public Car(string licensePlate, string passCode)
+        {
+            this.licensePlate = licensePlate;
+            this.passCode = passCode;
+        }
+        public void changePlace(int place)
+        {
+            this.place = place;
+        }
+        public void changeFloor()
+        {
+            this.floor = floor;
+        }
+    }
+}
